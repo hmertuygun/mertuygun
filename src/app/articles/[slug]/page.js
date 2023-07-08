d26c308a3591b9880e0d2ab9fce519a89b696649
Mert Uygun <uygun.hmert@gmail.com>
2023-07-08 15:46:50 +0300

    Fix Seo

--- src/app/articles/[slug]/page.js
+++ src/app/articles/[slug]/page.js
@@ -1,8 +1,10 @@
 "use client";
+
 import React, { Suspense, useEffect, useState } from "react";
 import cosmicConfig from "@/cosmic-config";
 import Loading from "@/app/articles/[slug]/loading";
 import ArticleContent from "@/components/ArticleContent";
+import Head from "next/head";
 
 export default function Article({ params }) {
   const [article, setArticle] = useState(null);
@@ -15,7 +17,6 @@ export default function Article({ params }) {
       })
       .props("slug,title,metadata")
       .depth(1);
-    console.log(data);
     return data;
   }
 
@@ -27,8 +28,36 @@ export default function Article({ params }) {
   }, [params]);
 
   return (
-    <Suspense fallback={<Loading />}>
-      {article ? <ArticleContent article={article} /> : null}
-    </Suspense>
+    <>
+      {article && (
+        <Head>
+          <title>{article.title} - Mert Uygun</title>
+          <meta
+            name="description"
+            content={article.metadata.description || ""}
+          />
+          <meta name="robots" content="index, follow" />
+          <meta
+            property="og:url"
+            content={`https://mertuygun.com/articles/${article.slug}`}
+          />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content={article.title} />
+          <meta
+            property="og:description"
+            content={article.metadata.description || ""}
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={article.title} />
+          <meta
+            name="twitter:description"
+            content={article.metadata.description || ""}
+          />
+        </Head>
+      )}
+      <Suspense fallback={<Loading />}>
+        {article ? <ArticleContent article={article} /> : null}
+      </Suspense>
+    </>
   );
 }
