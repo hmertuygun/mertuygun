37f73ae7c7883417bf64919fdfea494092b8403e
Mert Uygun <uygun.hmert@gmail.com>
2023-07-18 00:51:54 +0300

    Add Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { Providers } from "@/app/providers";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-4PM0F2C588" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-4PM0F2C588');
+        `}
+      </Script>
       <body className={inter.className}>
         <Providers>{children}</Providers>
         <Footer />
